8dfc5ca110d460c9e09faa9fb9e0170123cd2f80
DiandraRifqiM <luxamsky@gmail.com>
2025-05-13 11:09:31 +0700

    Del1 updated

--- del1.php
+++ del1.php
@@ -1,21 +1,32 @@
 <?php 
 
+    session_start();
+
+    // Check if user is logged in
+    if (!isset($_SESSION["login"])) {
+    header("Location: log.php");
+    exit;
+    }
+
+    // Get user ID from session
+    $userId = $_SESSION["user_id"];
+
     // Call Func
     require 'func.php';
 
     // Get Project and User id
     $id = (int)$_GET["id"];
-    $user_id = (int)$_GET["user_id"];
+    // $user_id = (int)$_GET["user_id"];
 
-    if(delProject($id, $user_id) > 0){
+    if(delProject($id, $userId) > 0){
         echo "<script>
                 alert('Project Deleted!');
-                document.location.href='index.php?id=$user_id'
+                document.location.href='index.php?id=$userId'
             </script>";        
     }else{
         echo "<script>
                 alert('Error!');
-                document.location.href='index.php?id=$user_id'
+                document.location.href='index.php?id=$userId'
             </script>";     
     }
 
